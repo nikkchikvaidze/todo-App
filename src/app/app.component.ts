8bce34ebf42779ae9a9f5c8a201d20bad8cfe036
nikkchikvaidze <72081766+nikkchikvaidze@users.noreply.github.com>
2022-03-20 23:34:47 +0400

    minor-updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface AddTask {
+  difficulty?: Difficulty;
+  text: string;
+}
+
+enum Difficulty {
+  Easy = 'Easy',
+  Medium = 'Medium',
+  Hard = 'Hard',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-App';
+  visible = false;
+  data: AddTask = {
+    difficulty: Difficulty.Easy,
+    text: '',
+  };
+  displayInputs() {
+    this.visible = true;
+  }
+
+  addTask() {
+    this.visible = false;
+  }
 }
+
+// <option [ngValue]=" null">Select</option>
+//                 <option [ngValue]=" difficulty.Easy" value="">Easy</option>
+//                 <option [ngValue]="difficulty.Medium" value="">Medium</option>
+//                 <option [ngValue]="difficulty.Hard" value="">Hard</option>
