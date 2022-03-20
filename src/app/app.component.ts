9ee86e689b830854b69e1f2b875f87c59422b8b2
nikkchikvaidze <72081766+nikkchikvaidze@users.noreply.github.com>
2022-03-21 02:10:43 +0400

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ interface AddTask {
   text: string;
 }
 
+interface Task {
+  text: string;
+  difficulty: Difficulty;
+}
+
 enum Difficulty {
   Easy = 'Easy',
   Medium = 'Medium',
@@ -19,20 +24,95 @@ enum Difficulty {
 })
 export class AppComponent {
   visible = false;
+  difficulty = Difficulty;
   data: AddTask = {
-    difficulty: Difficulty.Easy,
+    difficulty: undefined,
     text: '',
   };
+
+  todos: Task[] = [];
+  inprogress: Task[] = [];
+  done: Task[] = [];
+
   displayInputs() {
     this.visible = true;
   }
 
   addTask() {
+    if (!this.data.difficulty || !this.data.text) return;
+
+    this.todos = [
+      ...this.todos,
+      {
+        text: this.data.text,
+        difficulty: this.data.difficulty,
+      },
+    ];
+
+    this.data = {
+      difficulty: undefined,
+      text: '',
+    };
+
     this.visible = false;
   }
-}
 
-// <option [ngValue]=" null">Select</option>
-//                 <option [ngValue]=" difficulty.Easy" value="">Easy</option>
-//                 <option [ngValue]="difficulty.Medium" value="">Medium</option>
-//                 <option [ngValue]="difficulty.Hard" value="">Hard</option>
+  moveToProgress(task: Task, index: number) {
+    this.inprogress = [
+      ...this.inprogress,
+      {
+        text: task.text,
+        difficulty: task.difficulty,
+      },
+    ];
+
+    this.todos = this.todos.filter((_, i) => i !== index);
+  }
+
+  remove(index: number) {
+    this.todos = this.todos.filter((_, i) => i !== index);
+  }
+
+  moveToTodo(task: Task, index: number) {
+    this.todos = [
+      ...this.todos,
+      {
+        text: task.text,
+        difficulty: task.difficulty,
+      },
+    ];
+    this.inprogress = this.inprogress.filter((_, i) => i !== index);
+  }
+
+  moveToDone(task: Task, index: number) {
+    this.done = [
+      ...this.done,
+      {
+        text: task.text,
+        difficulty: task.difficulty,
+      },
+    ];
+    this.inprogress = this.inprogress.filter((_, i) => i !== index);
+  }
+
+  backToProgress(task: Task, index: number) {
+    this.inprogress = [
+      ...this.inprogress,
+      {
+        text: task.text,
+        difficulty: task.difficulty,
+      },
+    ];
+    this.done = this.done.filter((_, i) => i !== index);
+  }
+
+  emptyString = 'List is empty';
+
+  getStyle(task: Task) {
+    return {
+      easy: task.difficulty === Difficulty.Easy,
+      medium: task.difficulty === Difficulty.Medium,
+      hard: task.difficulty === Difficulty.Hard,
+    };
+  }
+}
